4ded9ea87b011bb2f7599fbc5f3fafe1b1f09251
alterego developer <alteregodeveloper@outlook.com>
2019-09-14 01:00:48 -0500

    Language files are loaded

---
+++ lang/en/vat.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @package   mod_observationtest
+ * @copyright 2019, alterego developer {@link https://alteregodeveloper.github.io}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+ $string['pluginname'] = 'Observation and attention';
+ $string['modulename'] = 'Observation and attention';
+ $string['observationtestname'] = 'Name';
+ $string['modulename_help'] = 'This module allows to exercise the student\'s visual attention and quantify it in three possible levels:
+* easy
+* middle
+* advanced';
+ $string['complexity'] = 'Complexity';
+ $string['easy'] = 'Easy';
+ $string['medium'] = 'Middle';
+ $string['advanced'] = 'Advanced';
+ 
---
+++ lang/es/vat.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @package   mod_observationtest
+ * @copyright 2019, alterego developer {@link https://alteregodeveloper.github.io}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+ $string['pluginname'] = 'Test de observación y atención';
+ $string['modulename'] = 'Test de observación y atención';
+ $string['observationtestname'] = 'Nombre';
+ $string['modulename_help'] = 'Éste módulo permite ejercitar la observación y atención del estudiante y cuantificarlo en tres posibles niveles:
+ * fácil
+ * intermedio
+ * avanzado';
+ $string['complexity'] = 'Complejidad';
+ $string['easy'] = 'Fácil';
+ $string['medium'] = 'Intermedio';
+ $string['advanced'] = 'Avanzado';
+ 
