69533e1768f0eaacf49575005ac10b341130f3db
alterego developer <alteregodeveloper@outlook.com>
2019-09-14 13:17:00 -0500

    Params in view are renamed

--- view.php
+++ view.php
@@ -10,20 +10,20 @@ require('../../config.php');
 require_once('lib.php');
  
 $id = required_param('id', PARAM_INT);
-list ($course, $cm) = get_course_and_cm_from_cmid($id, 'vat');
-$vat = $DB->get_record('vat', array('id'=> $cm->instance), '*', MUST_EXIST);
+list ($course, $cm) = get_course_and_cm_from_cmid($id, 'observationtest');
+$observationtest = $DB->get_record('observationtest', array('id'=> $cm->instance), '*', MUST_EXIST);
 
 require_login($course, true, $cm);
 $modulecontext = context_module::instance($cm->id);
 
-$PAGE->set_url('/mod/vat/view.php', array('id' => $cm->id));
-$PAGE->set_title(format_string($vat->name));
+$PAGE->set_url('/mod/observationtest/view.php', array('id' => $cm->id));
+$PAGE->set_title(format_string($observationtest->name));
 $PAGE->set_heading(format_string($course->fullname));
 $PAGE->set_context($modulecontext);
 
 echo $OUTPUT->header();
-echo $OUTPUT->heading($vat->name);
+echo $OUTPUT->heading($observationtest->name);
 
-echo $vat->intro;
+echo $observationtest->intro;
 
 echo $OUTPUT->footer();
