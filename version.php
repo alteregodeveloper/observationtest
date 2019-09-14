a85de198ef2f29e6bf575277838913645d211042
alterego developer <alteregodeveloper@outlook.com>
2019-09-13 21:43:03 -0500

    Copyright is updated

--- version.php
+++ version.php
@@ -16,7 +16,7 @@
  
 /**
  * @package   mod_observationtest
- * @copyright 2019, alterego developer < altgeregodeveloper@outlook.com >
+ * @copyright 2019, alterego developer {@link https://alteregodeveloper.github.io}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
  
