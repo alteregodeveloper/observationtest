9b90fab1abe51987e2f11fa8c4485544a463fac0
alterego developer <alteregodeveloper@outlook.com>
2019-09-27 14:09:58 -0500

    Improved exit button in add case form

--- localview/addcase_form.php
+++ localview/addcase_form.php
@@ -7,14 +7,15 @@
                 <div class="form-group col-10">
                     <select name="category" id="category" class="form-control">
                         <option value=""> - </option>
-                    <?php foreach($categories as $key => $category): ?>
+                        <?php foreach($categories as $key => $category): ?>
                         <option value="<?= $key ?>"><?= $category ?></option>
-                    <?php endforeach; ?>
+                        <?php endforeach; ?>
                     </select>
-                    <input type="hidden" name="categoryname" value="">       
+                    <input type="hidden" name="categoryname" value="">
                 </div>
                 <div class="form-group col-2">
-                    <a class="btn btn-primary" data-toggle="modal" data-target="#categoryModal"><i class="fas fa-plus"></i></a>
+                    <a class="btn btn-primary" data-toggle="modal" data-target="#categoryModal"><i
+                            class="fas fa-plus"></i></a>
                 </div>
             </div>
         </div>
@@ -22,9 +23,9 @@
             <label for="complexity">Complexity</label>
             <select name="complexity" id="complexity" class="form-control">
                 <option value=""> - </option>
-            <?php foreach($complexityranges as $key => $range): ?>
+                <?php foreach($complexityranges as $key => $range): ?>
                 <option value="<?= $key ?>"><?= $range ?></option>
-            <?php endforeach; ?>
+                <?php endforeach; ?>
             </select>
             <input type="hidden" name="complexityname" value="">
         </div>
@@ -32,43 +33,46 @@
     <div class="row">
         <div class="col"></div>
         <div class="col">
-        <label for="customFile">Image</label>
+            <label for="customFile">Image</label>
             <div class="custom-file">
                 <input type="file" name="customFile" class="custom-file-input" id="customFile" required>
                 <label class="custom-file-label" for="customFile">Choose file</label>
-                <small class="form-text text-muted">* Only support  jpeg, jpg, png or gif extension</small>
+                <small class="form-text text-muted">* Only support jpeg, jpg, png or gif extension</small>
             </div>
         </div>
     </div>
     <div class="row">
-        <div class="col text-right">
-            <p>&nbsp;</p>
+        <div class="col text-right mt-5">
+            <a href="<?= '//' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'] . '?id=' . $activity ?>"
+                class="btn btn-secondary">Exit</a>
             <button class="btn btn-primary" type="submit" id="add-case">Add new case</button>
         </div>
     </div>
 </form>
-<div class="modal fade" id="categoryModal" tabindex="-1" role="dialog" aria-labelledby="categoryModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="categoryModalLabel">New Category</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <div class="alert hide" role="alert"></div>
-        <form action="" method="POST">
-          <div class="form-group">
-            <label for="category">Category name</label>
-            <input type="text" name="category" id="newcategory" placeholder="new category name" class="form-control">
-          </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" id="add-category" class="btn btn-primary">Add category</button>
-      </div>
+<div class="modal fade" id="categoryModal" tabindex="-1" role="dialog" aria-labelledby="categoryModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="categoryModalLabel">New Category</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="alert hide" role="alert"></div>
+                <form action="" method="POST">
+                    <div class="form-group">
+                        <label for="category">Category name</label>
+                        <input type="text" name="category" id="newcategory" placeholder="new category name"
+                            class="form-control">
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" id="add-category" class="btn btn-primary">Add category</button>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
