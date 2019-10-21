ddc8a5c2b7107c78e4182926f62987d749e8b2e1
alterego developer <alteregodeveloper@outlook.com>
2019-10-21 14:12:19 -0500

    Mexico Spanish language file is added

---
+++ lang/es_mx/observationtest.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @package   mod_observationtest
+ * @copyright 2019, alterego developer {@link https://alteregodeveloper.github.io}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Test de atención visual';
+$string['modulename'] = 'Atención visual';
+$string['observationtest'] = 'Atención visual';
+$string['mod_observationtest'] = 'Atención visual';
+$string['observationtestname'] = 'Nombre';
+$string['modulename_help'] = 'Este módulo permite ejercitar la atención visual del alumno y cuantificarlo a través de tres posibles niveles:
+    * fácil
+    * medio
+    * avanzado';
+$string['name'] = 'Nombre';
+$string['category'] = 'Categoría';
+$string['new_category'] = 'Nueva categoría';
+$string['add_category'] = 'Agregar categoría';
+$string['category_name'] = 'Nombre de categoría';
+$string['new_category_name'] = 'nuevo nombre de categoría';
+$string['category_added'] = 'La categoría se agregó correctamente';
+$string['category_dont_added'] = 'No fue posible crear una nueva categoría';
+$string['complexity'] = 'Complejidad';
+$string['easy'] = 'Fácil';
+$string['medium'] = 'Medio';
+$string['advanced'] = 'Avanzado';
+$string['add_new'] = 'Agregar nuevo caso';
+$string['start_test'] = 'Comenzar el test';
+$string['end_test'] = 'Concluir el test';
+$string['result_is'] = 'Tu resultado es';
+$string['result_dont_save'] = 'No fue posible guardar el resultado';
+$string['question'] = 'Pregunta';
+$string['questions'] = 'Preguntas';
+$string['add_new_question'] = 'Agregar nueva pregunta';
+$string['question_added'] = 'Pregunta agregada con éxito';
+$string['question_dont_added'] = 'No fue posible crear una nueva pregunta';
+$string['answers'] = 'Respuestas';
+$string['checked_answer'] = 'La respuesta correcta está marcada';
+$string['error_answer'] = 'Se produjo un error al guardar las respuestas. Solo fue posible almacenar la primera';
+$string['file_supported'] = 'La extensión del archivo no es compatible. Seleccione una imagen con extensión jpeg, jpg, png o gif';
+$string['file_extension'] = 'Solo admite la extensión jpeg, jpg, png o gif';
+$string['file_saved'] = 'El archivo se guardó correctamente. Ahora puedes crear las preguntas';
+$string['try_image'] = 'Se produjo un error al intentar guardar la imagen. Inténtalo de nuevo';
+$string['image'] = 'Imagen';
+$string['choose_file'] = 'Selecciona la imagen';
+$string['exit'] = 'Salir';
+$string['close'] = 'Cerrar';
+$string['courseid_incorrect'] = 'El ID del curso es incorrecto';
